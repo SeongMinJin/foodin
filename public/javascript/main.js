ff108e3c898fc79f5c233900075551462f3e7479
fallen8 <sorksoek2@gmail.com>
2023-02-04 10:58:27 +0900

    implemented simple modal element

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,8 +1,9 @@
 var foodInfo = new Array();
 var modalOpen = false;
 
+
 window.onload = function() {
-	fetch('http://127.0.0.1:3000/data/foods.json')
+	fetch('http://192.168.0.3:3000/data/foods.json')
 	.then(response => response.json())
 	.then(data => {
 		Object.keys(data.maindishes).forEach(key => {
@@ -19,10 +20,9 @@ window.onload = function() {
 		container.classList.add('container');
 		container.style.left = Math.random() * 100 + 'vw';
 		container.style.animationDuration = duration + 's';
-	
+
+
 		image.classList.add('image');
-		image.style.width = "100px";
-		image.style.height = "100px";
 		image.style.animationDuration = duration + 's';
 		image.src = foodInfo[index].image;
 		container.addEventListener('click', function(event) {
@@ -31,7 +31,6 @@ window.onload = function() {
 				openModal(event, foodInfo[index])
 			}
 		});
-	
 	
 		container.appendChild(image);	
 		root.appendChild(container);
@@ -42,7 +41,12 @@ window.onload = function() {
 		}, duration * 1000);
 	
 	}
-	setInterval(mainDish, 500);
+
+	root.addEventListener('beforeunload', (event) => {
+		alert('out of focus!');
+	});
+
+	setInterval(mainDish, 600);
 	// mainDish();
 	});
 };
@@ -50,52 +54,65 @@ window.onload = function() {
 var openModal = function(event, foodInfo) {
 
 	var root = document.getElementById('root');
+	var curtain = document.createElement('div');
 	var modalBox = document.createElement('div');
-	var title = document.createElement('h4');
+	var closeButtonBox = document.createElement('div');
 	var closeButton = document.createElement('button');
+	var closeButtonIcon = document.createElement('img');
+	var titleBox = document.createElement('div');
+	var title = document.createElement('p');
+	var findButtonBox = document.createElement('div');
 	var findButton = document.createElement('button');
+	var findButtonText = document.createElement('p');
 
-
-	modalBox.style.width = '50vw';
-	modalBox.style.height = '60vh';
-	modalBox.style.position = 'relative';
-	modalBox.style.zIndex = '100';
-	modalBox.style.backgroundColor = 'pink';
-	modalBox.style.opacity = '0.8';
-	modalBox.style.display = 'flex';
-	modalBox.style.flexDirection = 'column';
-	modalBox.style.justifyContent = 'center';
-	modalBox.style.alignContent = 'center';
+	curtain.classList.add('curtain');
+	modalBox.classList.add('modal-box');
+	closeButtonBox.classList.add('close-button-box');
+	closeButton.classList.add('close-button');
+	closeButtonIcon.classList.add('close-button-icon');
+	titleBox.classList.add('title-box');
+	title.classList.add('title');
+	findButtonBox.classList.add('find-button-box');
+	findButton.classList.add('find-button');
+	findButtonText.classList.add('find-button-text');
 	
-
-	title.innerText = foodInfo.korean;
 	
+	curtain.addEventListener('click', function() {
+		modalOpen = false;
+		modalBox.remove();
+		curtain.remove();
+	});
 
-	closeButton.style.position = 'relative';
-	closeButton.style.width = '40%';
-	closeButton.style.height = '20%';
-	closeButton.innerHTML = '닫기';
+
 	closeButton.addEventListener('click', function(event) {
 		modalOpen = false;
+		curtain.remove();
 		modalBox.remove();
 	});
 
+	closeButtonIcon.src = '/images/icons/close.png';
+	title.innerText = foodInfo.korean;
+	
+	findButtonText.innerText = '내 주위 식당 찾기';
+	// findButton.addEventListener('click', function(event) {
+		// 	fetch('http://10.18.241.128:3000/find', {
+			// 		method: 'POST'
+	// 	})
+	// 	.then(data => {
+		// 		console.log(data.body);
+		// 	});
+		// });
+		
+	closeButtonBox.appendChild(closeButton);
+	closeButton.appendChild(closeButtonIcon);
+	titleBox.appendChild(title);
+	findButtonBox.appendChild(findButton);
+	findButton.appendChild(findButtonText);
 
-	findButton.style.position = 'relative';
-	findButton.style.width = '40%';
-	findButton.style.height = '20%';
-	findButton.innerHTML = '찾기';
-	findButton.addEventListener('click', function(event) {
-		fetch('http://10.18.241.128:3000/find', {
-			method: 'POST'
-		})
-		.then(data => {
-			console.log(data.body);
-		});
-	});
 
-	modalBox.appendChild(title);
-	modalBox.appendChild(closeButton);
-	modalBox.appendChild(findButton);
+	modalBox.appendChild(closeButtonBox);
+	modalBox.appendChild(titleBox);
+	modalBox.appendChild(findButtonBox);
 	root.appendChild(modalBox);
+	root.appendChild(curtain);
 }
--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
 	fs.readFile('public/html/index.html', 'utf-8', (err, data) => {
 		if (err) throw err;
-		// res.set('Content-Type', 'text/html');
+		res.set('Content-Type', 'text/html');
 		res.send(data);
 	});
 });
@@ -22,4 +22,4 @@ app.get('*', (req, res) => {
 	res.send('<h3>404 not found</h3>');
 })
 
-app.listen(3000, () => console.log('Foodin server is listening on port 3000'));
+app.listen(3000, '192.168.0.3', () => console.log('Foodin server is listening on port 3000'));
